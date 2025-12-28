0fb2a5fec1dcb307197b25389a2e3f1064ca9fdb
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-28 13:57:48 +0330

    add ball to play

--- Ping-pong/ping-pong.py
+++ Ping-pong/ping-pong.py
@@ -28,6 +28,15 @@ paddle_b.goto(350, 0)
 
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+
 
 # main game loop
 while True:
