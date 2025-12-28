2246c351af6cace2178858a2f95d64ffc8a3601d
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-28 13:53:57 +0330

    add paddles

---
+++ Ping-pong/ping-pong.py
@@ -0,0 +1,34 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Ping-pong by nanaqdi48@gmail.com")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+# Ball
+
+# main game loop
+while True:
+    wn.update()
